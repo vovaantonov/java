4857a49c726ac2030a668fdaf8a9ca3e73ceac2d
vovaantonov <speen1147@yandex.ru>
2022-02-14 22:53:06 +0300

    first java homework

---
+++ src/main/java/MyFirstClass.java
@@ -0,0 +1,8 @@
+public class MyFirstClass {
+
+    public static void main(String[] args) {
+        SecondClass secondClass = new SecondClass();
+        int result = secondClass.returnInt();
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/SecondClass.java
@@ -0,0 +1,8 @@
+public class SecondClass {
+    public static int firstField = 1234;
+
+    public int returnInt() {
+        System.out.println(firstField);
+        return firstField;
+    }
+}
