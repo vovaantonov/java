87b188c0793dd6ecab6ee3fb6e8af7704719e233
vovaantonov <speen1147@yandex.ru>
2022-02-15 13:56:40 +0300

    Исправление замечаний

--- src/main/java/MyFirstClass.java
+++ src/main/java/MyFirstClass.java
@@ -3,7 +3,7 @@ public class MyFirstClass {
     public static void main(String[] args) {
         SecondClass secondClass = new SecondClass();
         int intFromSecondClass = secondClass.returnInt();
-        String result = "~~~" + intFromSecondClass + "~~~";
-        System.out.println(result);
+        String formattedString = String.format("~~~%d~~~", intFromSecondClass);
+        System.out.println(formattedString);
     }
 }
--- src/main/java/SecondClass.java
+++ src/main/java/SecondClass.java
@@ -2,7 +2,7 @@ public class SecondClass {
     public static int firstField = 1234;
 
     public int returnInt() {
-        String modifiedField = "---" + firstField + "---";
+        String modifiedField = String.format("---%d---", firstField);
         System.out.println(modifiedField);
         return firstField;
     }
